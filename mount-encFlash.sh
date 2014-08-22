923392eea1db5de3a64c848347876b40fd44cef1
Colin Stevens <mail@colinjstevens.com>
2014-08-22 04:00:38 -0500

    Exit codes and consistent else statements

--- mount-encFlash.sh
+++ mount-encFlash.sh
@@ -12,7 +12,7 @@ enc_mount=""
 tc_filename=""
 fs=""
 
-if [ $EUID -ne 0 ]; then
+if [ $EUID -ne 0 ]; then # Check if the user is running script as root. If not, do so
 	exec sudo -- "$0" "$@"
 fi
 if [ "$1" == "mount" ]; then
@@ -22,38 +22,34 @@ if [ "$1" == "mount" ]; then
 	mount -t $fs -o rw -U $drive_uuid $drive_mount
 	if [ $? -ne 0 ]; then
 		echo "Drive mounting failed."
-		exit
+		exit 1
 	fi
 	cryptsetup --type tcrypt open $drive_mount/$tc_filename usbenc
 	mount -t $fs -o rw /dev/mapper/usbenc $enc_mount
 	if [ $? -ne 0 ]; then
 		echo "Failed to mount TrueCrypt container."
 		echo "Finished with errors."
-		exit
-	else
-		echo "Mount successful."
-		exit
+		exit 1
 	fi
+	echo "Mount successful."
 
 elif [ "$1" == "unmount" ]; then
 	echo "Unmounting..."
 	umount $enc_mount
 	if [ $? -ne 0 ]; then
 		echo "Failed to unmount drive."
-		exit
-	else
-		sleep 1 # Sleep buffers to give drives time to properly unmount and clean up
+		exit 1
 	fi
+	sleep 1 # Sleep buffers to give drives time to properly unmount and clean up
 	cryptsetup close usbenc
 	sleep 1
 	umount $drive_mount
 	if [ $? -ne 0 ]; then
 		echo "Failed to unmount TrueCrypt container."
 		echo "Finished with errors."
-		exit
-	else
-		echo "Unmount successful."
-	fi
+		exit 1
+	fi	
+	echo "Unmount successful."
 else
 	echo "Bad arg. Accepted args are: mount, unmount"
 fi
